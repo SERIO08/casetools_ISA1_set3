eb90c879b455f0cc9257344b279e9edbc8faa855
serio <seriobraganca01@gmail.com>
2024-02-08 09:42:30 +0530

    commit

---
+++ casetools_ISA1_set3/src/casetools_ISA1_set3/movie.java
@@ -0,0 +1,15 @@
+package casetools_ISA1_set3;
+
+public class movie {
+	private String title;
+private String director;
+public movie(String title,String director) {
+	this.title=title;
+	this.director=director;
+}
+public String gettitle() {
+	return this.title;
+}
+
+
+}
